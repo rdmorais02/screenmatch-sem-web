e670a3b2b61297382a3280e5551f7beb15088e49
Rodrigo Morais <rodrigomoraisdasilva2@gmail.com>
2024-07-10 21:18:44 -0300

    inicio proje to Spring

---
+++ src/test/java/br/com/alura/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
